5fc3d8b667b48dd39919ef48b0d3cab90d9ea9c8
JuliaGrisales <jgrisales@gmail.com>
2020-11-19 00:07:57 +0100

    Dimanic route for show details of a cripto currency

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
     <h1 class="text-gray-700 text-6xl">fresh.solutions Exchange v1.0.0</h1>
     <p class="text-gray-600 text-xl text-center">
       Project to get the most important cripto currencies from Coincap API REST.
-      <br />This project is using the basic course of Vue.js Basico of Platzi
+      <br />This project is using the basic course of Vue.js of Platzi
     </p>
     <router-link class="mt-5 text-xl text-green-600 hover:underline" to="/">
       Back to home page
