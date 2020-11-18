423149bf1ffd138ba013cd79ffb8d5cc7e8a5e6a
JuliaGrisales <jgrisales@gmail.com>
2020-11-18 18:29:47 +0100

    router-view, router-link

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">fresh.solutions Exchange v1.0.0</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Project to get the most important cripto currencies from Coincap API REST.
+      <br />This project is using the basic course of Vue.js Basico of Platzi
+    </p>
+    <router-link class="mt-5 text-xl text-green-600 hover:underline" to="/">
+      Back to home page
+    </router-link>
+  </div>
+</template>
